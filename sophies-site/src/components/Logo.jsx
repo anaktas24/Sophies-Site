78a254b23efb6e7f53690e3fe981db545f42ad72
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-25 22:02:10 +0200

    footer

--- sophies-site/src/components/Logo.jsx
+++ sophies-site/src/components/Logo.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import '../styles/Logo.css';
 
-
-function Logo(){
+function Logo() {
   return (
-    <div>
-      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css" />
-      <div className="box">
-        <span className="logo">
-          <i className="i">C</i>
-          <i className="x">S</i>
-        </span>
-
-      </div>
+    <div className="box">
+      <span className="logo">
+        <i className="i">C</i>
+        <i className="x">S</i>
+      </span>
     </div>
   );
 }
--- sophies-site/src/components/Navbar.jsx
+++ sophies-site/src/components/Navbar.jsx
@@ -1,39 +1,55 @@
-// src/components/Navbar/Navbar.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-import Logo from './Logo'
+import Logo from './Logo';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
-    <nav className="navbar">
-      <Link to="/" className="brand" >
-        <Logo/>
-      </Link>
-      <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="hamburger"></div>
-      </div>
-      <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <li>
-          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
-        </li>
-        <li>
-          <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
-        </li>
-        <li>
-          <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
-        </li>
-        <li>
-          <Link to="/portfolio" onClick={() => setMenuOpen(false)}>Portfolio</Link>
-        </li>
-      </ul>
-    </nav>
+    <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+    <Link to="/" className={`brand ${isScrolled ? 'scrolled' : ''}`}>
+      {!isScrolled ? <Logo /> : <span className="text">Sonya Studio</span>}
+    </Link>
+    <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+      <div className="hamburger"></div>
+      <div className="hamburger"></div>
+      <div className="hamburger"></div>
+    </div>
+    <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+      <li>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+      </li>
+      <li>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+      </li>
+      <li>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+      </li>
+      <li>
+        <Link to="/portfolio" onClick={() => setMenuOpen(false)}>Portfolio</Link>
+      </li>
+    </ul>
+  </nav>
+
   );
 }
 
