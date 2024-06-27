7d7c6a63eb82aeaf1647b224fd439a696ff4b58e
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-27 23:04:36 +0200

    logo

--- sophies-site/src/App.jsx
+++ sophies-site/src/App.jsx
@@ -1,13 +1,12 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Line from './components/Line';
 import './App.css';
 
 
